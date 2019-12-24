aac4fc1d97c4439bdae36ffa53acbf6f7efa5a38
Ricky Laney <rlaneyjr@gmail.com>
2019-12-24 11:41:20 -0500

    Initial commit

---
+++ get_files.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash -e
+
+
+wget --no-check-certificate https://10.0.20.101:443/software/avctKVM.jar
+wget --no-check-certificate https://10.0.20.101:443/software/avctKVMIOMac64.jar
+wget --no-check-certificate https://10.0.20.101:443/software/avctVMMac64.jar
